07cdb9cbeac8ba3fc7811df20c8c1dcf1d8ef85a
Xingyu (Tim) Chen <46166892+XingyuChenTim@users.noreply.github.com>
2023-03-06 17:25:30 -0700

    update hier

--- arm.R
+++ arm.R
@@ -33,17 +33,33 @@ FoodsKumar <- read.transactions("new_arm_for_r.csv",
 inspect(FoodsKumar)
 
 ##### Use apriori to get the RULES
-FrulesK = arules::apriori(FoodsKumar, parameter = list(support=.35, 
-                                                       confidence=.5, minlen=2))
+FrulesK = arules::apriori(FoodsKumar, parameter = list(support=.01, 
+                                                       confidence=.1, minlen=2))
 inspect(FrulesK)
 
 ## Plot of which items are most frequent
 itemFrequencyPlot(FoodsKumar, topN=20, type="absolute")
 
 ## Sort rules by a measure such as conf, sup, or lift
-SortedRulesK <- sort(FrulesK, by="confidence", decreasing=TRUE)
-# inspect(SortedRulesK[1:10])
-(summary(SortedRulesK))
+Sortedconf <- sort(FrulesK, by="confidence", decreasing=TRUE)
+inspect(Sortedconf[1:15])
+
+## Sort rules by a measure such as conf, sup, or lift
+Sortedsup <- sort(FrulesK, by="support", decreasing=TRUE)
+inspect(Sortedsup[1:15])
+
+## Sort rules by a measure such as conf, sup, or lift
+Sortedlift <- sort(FrulesK, by="lift", decreasing=TRUE)
+inspect(Sortedlift[1:15])
+
+# (summary(SortedRulesK))
+
+## Selecting or targeting specific rules  RHS
+appRules <- apriori(data=FoodsKumar,parameter = list(supp=.001, conf=.01, minlen=2),
+                     appearance = list(default="lhs", rhs="app"),
+                     control=list(verbose=FALSE))
+appRules <- sort(appRules, decreasing=TRUE, by="confidence")
+inspect(appRules[1:4])
 
 ## Selecting or targeting specific rules  RHS
 goodRules <- apriori(data=FoodsKumar,parameter = list(supp=.001, conf=.01, minlen=2),
@@ -53,11 +69,11 @@ goodRules <- sort(goodRules, decreasing=TRUE, by="confidence")
 inspect(goodRules[1:4])
 
 ## Selecting rules with LHS specified
-BreadRules <- apriori(data=FoodsKumar,parameter = list(supp=.001, conf=.01, minlen=2),
-                      appearance = list(default="rhs", lhs="Bread"),
+niceRules <- apriori(data=FoodsKumar,parameter = list(supp=.001, conf=.01, minlen=2),
+                      appearance = list(default="rhs", lhs="nice"),
                       control=list(verbose=FALSE))
-BreadRules <- sort(BreadRules, decreasing=TRUE, by="support")
-inspect(BreadRules[1:4])
+niceRules <- sort(niceRules, decreasing=TRUE, by="support")
+inspect(niceRules[1:4])
 
 ## Visualize
 ## tcltk
